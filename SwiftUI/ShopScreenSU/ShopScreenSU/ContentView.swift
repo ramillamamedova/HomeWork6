c050dc5ea60f20fdfcd9f9d48797fc473c62690c
ramillamamedova <114265678+ramillamamedova@users.noreply.github.com>
2024-12-11 21:34:44 +0200

    empty projects were added

---
+++ SwiftUI/ShopScreenSU/ShopScreenSU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopScreenSU
+//
+//  Created by Ram on 11.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/ShopScreenSU/ShopScreenSU/ShopScreenSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopScreenSUApp.swift
+//  ShopScreenSU
+//
+//  Created by Ram on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopScreenSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
